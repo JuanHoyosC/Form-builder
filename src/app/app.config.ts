7d45fc364b9a9c5b45ea029b7804c8d99bc75f72
Juan Carlos Hoyos <juan.hoyos@integrait.co>
2024-10-24 00:40:00 -0500

    👷add primeng files and ionic icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormlyModule } from '@ngx-formly/core';
 
 import { routes } from './app.routes';
+import { InputFieldType } from './formly/components/formly-textarea/input';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom([
+      FormlyModule.forRoot({ types: [{ name: 'input', component:  InputFieldType}] }),
+      BrowserModule,
+      BrowserAnimationsModule
+    ]),
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import {
+  provideNgIconsConfig,
+} from '@ng-icons/core';
+bootstrapApplication(AppComponent, {
+  providers: [
+    appConfig.providers,
+    provideNgIconsConfig({}),
+  ],
+}).catch((err) => console.error(err));
