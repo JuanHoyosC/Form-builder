12ae10009661defa1212ae10cd77f07686ff6a26
Juan Carlos Hoyos <juan.hoyos@integrait.co>
2024-11-08 23:50:39 -0500

    ✨add phone field

--- src/app/pages/form-builder/fields/input-field-tel/input-field-tel.component.ts
+++ src/app/pages/form-builder/fields/input-field-tel/input-field-tel.component.ts
@@ -1,13 +1,32 @@
 import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FieldType, FieldTypeConfig } from '@ngx-formly/core';
 import { InputMaskModule } from 'primeng/inputmask';
-
+import { LabelTooltipComponent } from '../../../../shared/components/label-tooltip/label-tooltip.component';
+import { FieldDescriptionComponent } from '../../../../shared/components/field-description/field-description.component';
+import { CommonModule } from '@angular/common';
+import { CountryISO,  IntlInputTelComponent } from 'p-intl-input-tel';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PhoneNumberFormat } from 'google-libphonenumber';
 @Component({
   selector: 'app-input-field-tel',
   standalone: true,
-  imports: [InputMaskModule],
+  imports: [ CommonModule, FieldDescriptionComponent, InputMaskModule, IntlInputTelComponent, LabelTooltipComponent, ReactiveFormsModule],
   schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './input-field-tel.component.html',
-  styleUrl: './input-field-tel.component.css'
+  styleUrl: './input-field-tel.component.scss'
 })
-export class InputFieldTelComponent extends FieldType<FieldTypeConfig> {}
+export class InputFieldTelComponent extends FieldType<FieldTypeConfig> {
+  public readonly CountryISO = CountryISO;
+  public readonly PhoneNumberFormat = PhoneNumberFormat;
+  public readonly SearchCountryField = SearchCountryField;
+
+  public separateDialCode: boolean = true;
+  public favoriteCountries: CountryISO[] = [];
+  public phone = new FormControl({ value: '', disabled: false }, [ Validators.required ]);
+}
+
+export enum SearchCountryField {
+  DIALCODE = 'dialCode',
+  ISO2 = 'iso2',
+  NAME = 'name',
+};
